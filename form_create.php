c3bd2b21fd87906c4689fc8962af86a11affd036
yapsterr <60122561+yapsterr@users.noreply.github.com>
2020-03-02 22:54:33 +0800

    Delete form_create.php

--- form_create.php
+++
@@ -1,8 +0,0 @@
-<form action="pro_create.php" method="POST">
-<input type="text" name="name" placeholder="name"/>
-<input type="text" name="description" placeholder="description"/>
-<input type="text" name="price" placeholder="price"/>
-<input type="text" name="category" placeholder="category"/>
-<input type="submit" name="submit" value="submit"/>
-
-</form>
